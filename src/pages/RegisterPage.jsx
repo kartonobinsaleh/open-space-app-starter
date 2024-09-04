a8b819af5c2b4a3b0f363819f39977473b0eed0e
Kartono <Kartono.saleh@gmail.com>
2024-09-04 13:32:32 +0700

    menerapkan asyncRegisterUser di halaman register

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,31 +1,33 @@
-import React from 'react';
-import { IoEarthOutline } from 'react-icons/io5';
-import { Link, useNavigate } from 'react-router-dom';
-import RegisterInput from '../components/RegisterInput';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { IoEarthOutline } from "react-icons/io5";
+import { Link, useNavigate } from "react-router-dom";
+import { asyncRegisterUser } from "../states/users/action";
+import RegisterInput from "../components/RegisterInput";
 
 function RegisterPage() {
   const navigate = useNavigate();
-  const dispatch = null; // @TODO: get dispatch function from store
+  const dispatch = useDispatch();
 
   const onRegister = ({ name, id, password }) => {
-    // @TODO: dispatch async action to register
+    dispatch(asyncRegisterUser({ id, name, password }));
 
-    navigate('/');
+    navigate("/");
   };
 
   return (
     <section className="register-page">
       <header className="register-page__hero">
-        <h1><IoEarthOutline /></h1>
+        <h1>
+          <IoEarthOutline />
+        </h1>
       </header>
       <article className="register-page__main">
         <h2>Create your account</h2>
         <RegisterInput register={onRegister} />
 
         <p>
-          Already have an account?
-          {' '}
-          <Link to="/">Login</Link>
+          Already have an account? <Link to="/">Login</Link>
         </p>
       </article>
     </section>
