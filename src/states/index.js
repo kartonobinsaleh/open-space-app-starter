b3753fa0e723c1735e1ce3941a5da849dcd61548
Kartono <Kartono.saleh@gmail.com>
2024-09-04 13:31:04 +0700

    membuat redux store dengan configureStore

--- src/states/index.js
+++ src/states/index.js
@@ -1,3 +1,21 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authUserReducer from "./authUser/reducer";
+import isPreloadReducer from "./isPreload/reducer";
+import usersReducer from "./users/reducer";
+import talksReducer from "./talks/reducer";
+import talkDetailReducer from "./talkDetail/reducer";
+
 /**
  * @TODO: Create Redux store
  */
+const store = configureStore({
+  reducer: {
+    authUser: authUserReducer,
+    isPreload: isPreloadReducer,
+    users: usersReducer,
+    talks: talksReducer,
+    talkDetail: talkDetailReducer,
+  },
+});
+
+export default store;
