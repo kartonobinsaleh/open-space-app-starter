9ec5d768d716fff73e584c11f88394673e788ca9
Kartono <Kartono.saleh@gmail.com>
2024-09-04 13:33:17 +0700

    menerapkan asyncPopulateUsersAndTalks, asyncToogleLikeTalk, asyncAddTalk di halaman home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,28 +1,27 @@
-import React, { useEffect } from 'react';
-import TalkInput from '../components/TalkInput';
-import TalksList from '../components/TalksList';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import TalkInput from "../components/TalkInput";
+import TalksList from "../components/TalksList";
+import { asyncPopulateUsersAndTalks } from "../states/shared/action";
+import { asyncAddTalk, asyncToogleLikeTalk } from "../states/talks/action";
 
 function HomePage() {
-  const {
-    talks = [],
-    users = [],
-    authUser,
-  } = {}; // @TODO: get talks, users, and authUser state from store
+  const talks = useSelector((state) => state.talks || []);
+  const users = useSelector((state) => state.users || []);
+  const authUser = useSelector((state) => state.authUser || {});
 
-  const dispatch = null; // @TODO: get dispatch function from store
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    // @TODO: dispatch async action to populate talks and users data
-
+    dispatch(asyncPopulateUsersAndTalks());
   }, [dispatch]);
 
   const onAddTalk = (text) => {
-    // @TODO: dispatch async action to add talk
-
+    dispatch(asyncAddTalk({ text }));
   };
 
   const onLike = (id) => {
-    // @TODO: dispatch async action to toggle like talk
+    dispatch(asyncToogleLikeTalk(id));
   };
 
   const talkList = talks.map((talk) => ({
